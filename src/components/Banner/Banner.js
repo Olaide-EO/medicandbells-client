7276ec1713330d5831686d1def1be7850e8ac03c
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-16 18:40:17 +0100

    add banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,28 +1,35 @@
 import React from "react";
-import "./Greeting.css";
-import SocialMedia from "../../components/socialMedia/SocialMedia";
-import Button from "../../components/button/Button";
-import { greeting } from "../../portfolio";
+import "./Banner.css";
+import SocialMedia from "../socialMedia/SocialMedia";
+import Button from "../button/Button";
+//import { greeting } from "../../portfolio";
 
 export default function Banner() {
   return (
     <div className="greet-main" id="greeting">
+      
       <div className="greeting-main">
         <div className="greeting-text-div">
           <div>
-            <h1 className="greeting-text">{greeting.title}</h1>
-            <p className="greeting-text-p subTitle">{greeting.subTitle}</p>
+            <h1 className="greeting-text">Stay on schedule, once and for all</h1>
+            <p className="greeting-text-p subTitle">Busy and can't keep up with schedule? We provide instant  notification for your prescription schedule</p>
             <SocialMedia />
             <div className="button-greeting-div">
-              <Button text="Contact me" href="#contact" />
-              <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
+              <Button text="Login" href="/login" />
+              <Button text="Signup"  href="/signup" />
             </div>
           </div>
         </div>
+
+
         <div className="greeting-image-div">
-          <img alt="E.O sitting on table" src={require("../../assests/images/manOnTable.svg")}></img>
+          <img alt="E.O sitting on table" src={require("../../assests/images/undraw_season_change_f99v.svg")}></img>
         </div>
+
       </div>
+
+      
     </div>
   );
 }
+
