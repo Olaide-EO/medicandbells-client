6657690e6cbb5ee46b6e295eeff60880b2b8f69d
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-17 10:02:56 +0100

    add socio rule

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./SocialMedia.css";
-import { socialMediaLinks } from "../../portfolio";
+//import { socialMediaLinks } from "../../portfolio";
 
 export default function socialMedia() {
   return (
     <div className="social-media-div">
-      <a href={socialMediaLinks.github} className="icon-button github" target="_blank">
+      <a href="#" className="icon-button github">
         <i className="fab fa-github"></i>
         <span></span>
       </a>
-      <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank">
+      <a href="#" className="icon-button linkedin">
         <i className="fab fa-linkedin-in"></i>
         <span></span>
       </a>
-      <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank">
+      <a href="#" className="icon-button google" >
         <i className="fab fa-google"></i>
         <span></span>
       </a>
-      <a href={socialMediaLinks.gitlab} className="icon-button gitlab" target="_blank">
+      <a href="#" className="icon-button gitlab" >
         <i className="fab fa-gitlab"></i>
         <span></span>
       </a>
-      <a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank">
+      <a href="#" className="icon-button facebook" >
         <i className="fab fa-facebook-f"></i>
         <span></span>
       </a>
