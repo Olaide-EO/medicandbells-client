be4bf4eb8a85e4b8f01a87de3ba3629f2f86fb12
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-18 18:25:26 +0100

    check if authenticated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,22 +7,15 @@ import "./Navbar.css";
 import { useSelector } from 'react-redux';
 
 
-
 export default function Navbar(props) {
+
+  let stateForNavbar = useSelector(state => state.user);
   
-  return (
-    
-        <header className="header">
-        <Link to="/" className="logo">
-          
-          <span className="logo-name">Meds <span className="and">&</span> Bells</span>
-         
-        </Link>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" htmlFor="menu-btn">
-          <span className="navicon"></span>
-        </label>
-        <ul className="menu">
+  let dataState = useSelector(state => state.data);
+
+  let navLink = !stateForNavbar.authenticated ? (
+
+          <ul className="menu">
           
           <li>
             <Link to='/login'>Login</Link>
@@ -37,6 +30,39 @@ export default function Navbar(props) {
             <Link to='/contactus'>Contact Us</Link>
           </li>
         </ul>
+    ) : (
+       
+           <ul className="menu">
+          
+          <li>
+            <Link to='/medications'>Medications</Link>
+          </li>
+          <li>
+            <Link to='/add'>Add New</Link>                                                       
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='/Faq'>Faq</Link>
+          </li>
+        </ul>
+
+    )
+  
+  return (
+    
+        <header className="header">
+        <Link to="/" className="logo">
+          
+          <span className="logo-name">Meds <span className="and">&</span> Bells</span>
+         
+        </Link>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon"></span>
+        </label>
+        {navLink}
       </header>
 
 
