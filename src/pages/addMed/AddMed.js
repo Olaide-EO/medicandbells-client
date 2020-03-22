9495bbf26863a603e619eba6e4800834f9716561
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-22 12:15:30 +0100

    work in progress

--- src/pages/addMed/AddMed.js
+++ src/pages/addMed/AddMed.js
@@ -1,19 +1,99 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 
+import MedName from './MedName';
+import ReminderTimes from './ReminderTimes';
+import './AddMed.css';
 
-class AddMed extends Component {
 
-    render() {
-        
-        return (
-          <div>
-          Hello From Add One Medication
+const styles = (theme) => ( {
+    
+    root: {
+        flexGrow: 1,
+        flexWrap: "wrap"
+      },
+ 
+}
+)
+
+class AddMed extends React.Component {
+  
+ constructor(){
+       super();
+       this.state = {
+           medicationName: '',
+           setReminder: false,
+           reminderTimes: [],
+           schedule: 'everyday',
+           setReminderTime: false
+
+       }
+   }
+
+
+   nameHandler = (val) => {
+    this.setState({
+      medicationName: val
+    })
+  }
+
+  handleNext = () => {
+  	if(this.state.medicationName !== ''){
+  		this.setState({
+  			setReminderTime: true
+  		})
+  	}
+  }
+
+  render(){
+
+       return (
+
+
+  
+            
+  	<div className="addMedContainer">
+  	     <div className="addMed">
+              <Grid id="productGrid" item xs={12} sm={8} md={6} lg={4} xl={3}  > <MedName nameHandler={this.nameHandler} />  </Grid>
+  	
+             
+             {this.state.setReminderTime && (
+             	 <Grid id="productGrid" item xs={10} sm={8} md={6} lg={4} xl={3}  > <ReminderTimes/>  </Grid>
+  	
+             	
+             	)}
+             
+             <Grid id="productGrid" item xs={10} sm={8} md={6} lg={4} xl={3}  > 
+             
+              <div id="cardActions">
+                  <Button 
+                       disabled={this.state.medicationName === ''}   
+                       size="medium" 
+                       variant="contained" 
+                       onClick={this.handleNext}
+                       color="primary"
+                       >
+                       Next
+                   </Button>
+              </div>
+
+               </Grid>
+  	
+             
          </div>
-
-        )
-    }
+    </div>
+  );	
+  }
+ 
 }
 
 
-export default AddMed;
+
+export default withStyles(styles)(AddMed);
+
