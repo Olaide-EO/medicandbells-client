93dd01e00906bdc5ea8475e0f7f1102773963947
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-25 10:54:52 +0100

    convert decimal to hour

--- src/pages/addMed/DecimalToHour.js
+++ src/pages/addMed/DecimalToHour.js
@@ -4,7 +4,14 @@
 export default function DecimalToHour(val){
 	
 	let dig = Math.floor(val).toString();
-    let n = `${dig}:00`;
+	let n;
+	if(val < 10){
+	     n = `0${dig}:00`;
+	}else{
+		n = `${dig}:00`
+	}
+
+    
    
     return n;
 
