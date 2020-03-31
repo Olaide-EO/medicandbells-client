3b58c0cc332313518c90b6a9214ab7e1dc1bac42
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-31 14:10:54 +0100

    change layout

--- src/pages/viewMed/ViewMed.js
+++ src/pages/viewMed/ViewMed.js
@@ -1,19 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+
+import SingleMed from './SingleMed'
+
+import { connect } from 'react-redux';
+
+
+const styles = (theme) => ({
+  root: {
+    padding: theme.spacing(1)
+  }
+});
+
+class ViewMed extends React.Component{
+
+  
+  
+
+  render(){
+   
+   const { classes } = this.props;
+   const { medication } = this.props.user;
+
+  let medIndex = medication.findIndex(
+               (med) => med.medId === this.props.match.params.medId );
+
+      let newMed = medication[medIndex];
+
+	  
+	   let   myMedCrop = JSON.parse(JSON.stringify(newMed));
+	  delete myMedCrop.medName;
+	  delete myMedCrop.medId;
+	  delete myMedCrop.createdAt;
+	  delete myMedCrop.userId
+	 
+
+  	 return (
+    <div className={classes.root}>
+      
+          <SingleMed />
+        {console.log(newMed)}
+        {console.log(myMedCrop)}
+    </div>
+  );
+  }
+ 
+};
+
+
+const mapStateToProps = (state) => ({
+    user: state.user,
+    data: state.data
+})
 
 
 
-class ViewMed extends Component {
 
-    render() {
-        
-        return (
-          <div>
-          Hello From View One Medication Component
-         </div>
-
-        )
-    }
-}
-
-
-export default ViewMed;
+export default connect(mapStateToProps)(withStyles(styles)(ViewMed));
